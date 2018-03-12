2c969d4797d1d5c871a02e34af755fbbfda7e543
Frank Palafox <frank.palafox@stu.jefferson.kyschools.us>
2018-03-12 08:00:10 -0400

    first commit

---
+++ genremedia.py
@@ -0,0 +1,29 @@
+#NO U#
+
+review_material = []
+
+media_type = input("What Media type is it? ")
+review_material.append(media_type)
+
+genre = input("What Genre is it? ")
+review_material.append(genre)
+
+title = input("Whats the title of the Media? ") 
+review_material.append(title)
+
+year_creation = input("What year was it created? ")
+review_material.append(year_creation)
+
+description = input("Can you say something interesting about this Media? ")
+review_material.append(description)
+
+rating = float(input("On a scale of 1 to 10 what do you rate this media? "))
+review_material.append(rating)
+
+print (review_material) 
+
+
+
+
+
+
