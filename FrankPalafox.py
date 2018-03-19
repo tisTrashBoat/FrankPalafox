00002b648c96093bfdc7febf418652b1b4293227
Frank Palafox <frank.palafox@stu.jefferson.kyschools.us>
2018-03-19 08:00:03 -0400

    commit first line

---
+++ FrankPalafox.py
@@ -0,0 +1,21 @@
+music_terms = {
+    "Tempo" : "a set number that determines the speed of the song and is usually set in beats per minute.",\
+    
+    "Rhythm" :  "movement marked by the regulated succession of strong and weak elements, or of opposite or different conditions.",\
+    
+    "Arrangment" : "an arrangement is a musical reconceptualization of a previously composed work.It may differ from the original work by means of reharmonization, melodic paraphrasing, orchestration, or development of the formal structure.",\
+    
+    "Cadence" : "A harmonic cadence is a progression of (at least) two chords that concludes a phrase, section, or piece of music.",\
+    
+    "Syncopation": "Syncopation involves a variety of rhythms which are in some way unexpected which make part or all of a tune or piece of music off-beat.",\
+    
+    "Duet" : "a musical composition for two performers in which the performers have equal importance to the piece.",\
+}
+
+
+print "Rhythm" in music_terms
+
+for key in music_terms:
+    print key + " : " + music_terms[key]
+
+    
