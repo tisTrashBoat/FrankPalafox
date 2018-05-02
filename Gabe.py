283c3914ee4632d03ca456763f7f671cc48d2ac4
Frank Palafox <frank.palafox@stu.jefferson.kyschools.us>
2018-05-02 08:13:00 -0400

    commit stage 1

---
+++ Gabe.py
@@ -0,0 +1,34 @@
+species = "Doggo"
+origin = "America"
+the_type = "Eskimo"
+size = "Smol"
+age = "6"
+bork_type= "Angery"
+followers= "300,000+"
+origin="Youtube"
+#In,Mph#
+x=30-int(age)
+y=0
+z=-20
+#In cups#
+eating=2
+y=0
+def walk_bork(Speed):
+    if x<=Speed and size=="Smol":
+        print ("Doggo does a trip")
+    if x>Speed and size =="Smol":
+        print ("Doggo does a heckin happy")
+    if z>Speed and size=="Smol":
+        print ("Doggo does a trip")
+    if Speed==y:
+        print ("Doggo does a shleep")
+def bork_eating(Amount):
+    if size=="Smol" and Amount>=eating:
+        print ("Doggo overstuffed")
+    if size=="Smol" and Amount<eating and Amount>.5:
+        print ("Doggo happy boi")
+    if size=="Smol" and Amount<=y:
+        print ("Doggo does an angery")
+
+   
+        
